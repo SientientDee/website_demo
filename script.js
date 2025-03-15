fab01eb523150baf808b20829b109d411bdfa2e7
SientientDee <thelazyrobot007@hotmail.com>
2025-03-15 08:06:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,34 +23,3 @@ document.getElementById('colors-btn').addEventListener('click', () => {
   bg.spheres.setColors([0xffffff * Math.random(), 0xffffff * Math.random(), 0xffffff * Math.random()])
 })
 
-// Function to handle device orientation and dispatch a synthetic mousemove event
-function handleOrientation(event) {
-  // Map device orientation angles to canvas coordinates:
-  // event.gamma (left/right tilt, typically in [-90, 90]) is mapped to the x-coordinate.
-  // event.beta (front/back tilt, typically in [-90, 90]) is mapped to the y-coordinate.
-  const x = (event.gamma + 90) / 180 * window.innerWidth;
-  const y = (event.beta + 90) / 180 * window.innerHeight;
-  
-  // Create and dispatch a synthetic mousemove event to the canvas
-  const simulatedEvent = new MouseEvent('mousemove', {
-    clientX: x,
-    clientY: y
-  });
-  document.getElementById('webgl-canvas').dispatchEvent(simulatedEvent);
-}
-
-// Request permission for device orientation events on iOS (if necessary), otherwise just add the event listener
-if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-  // For iOS 13+ devices
-  DeviceOrientationEvent.requestPermission()
-    .then(response => {
-      if (response === 'granted') {
-        window.addEventListener('deviceorientation', handleOrientation);
-      }
-    })
-    .catch(console.error);
-} else {
-  // For other devices that don't require permission
-  window.addEventListener('deviceorientation', handleOrientation);
-}
-
